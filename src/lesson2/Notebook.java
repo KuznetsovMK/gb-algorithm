7f0eaeb85b24c5ca51ab5463a5548cadcb81b5d3
Кузнецов Михаил Владимирович <kuznetsov.m@russianpost.ru>
2022-02-24 21:16:50 +0300

    lesson2: hw

--- src/lesson2/Notebook.java
+++ src/lesson2/Notebook.java
@@ -1,7 +1,38 @@
 package lesson2;
 
+import java.util.Objects;
+
 public class Notebook {
     private int price;
     private int memory;
     private String manufacturer;
+
+
+    public Notebook(int price, int memory, String manufacturer) {
+        this.price = price;
+        this.memory = memory;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    @Override
+    public String toString() {
+        return "Notebook{" +
+                "price=" + price +
+                ", memory=" + memory +
+                ", manufacturer='" + manufacturer + '\'' +
+                '}';
+    }
+
 }
