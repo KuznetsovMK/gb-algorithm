07baafc589dc7f502a672b7ffe81b18b81f88bcf
Кузнецов Михаил Владимирович <kuznetsov.m@russianpost.ru>
2022-02-27 22:31:22 +0300

    изменен алгоритм поиска

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -1,5 +1,6 @@
 package lesson3;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,7 +17,7 @@ public class Main {
         Integer[] arr2 = {1, 2, 4};
         Integer[] arr3 = {1, 2, 3, 4, 5, 7, 8};
         Integer[] arr4 = {1, 2, 4, 5, 6, 7, 8};
-        Integer[] arr5 = {1, 2 ,3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16};
+        Integer[] arr5 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16};
         Integer[] arr6 = {1, 2, 4, 5, 6};
         Integer[] arr7 = {};
 
@@ -32,18 +33,6 @@ public class Main {
         test(map);
     }
 
-    public static void test(Map<Integer[], Integer> map) {
-
-        for (Map.Entry<Integer[], Integer> o : map.entrySet()) {
-            Integer[] arr = o.getKey();
-            Integer expected = o.getValue();
-            Integer actual = actualSum(arr);
-
-            System.out.println("Ожидаемый результат: " + expected + ", Полученный результат: " + actual);
-
-        }
-    }
-
     public static int actualSum(Integer[] arr) {
         int result = 0;
         for (int j : arr) {
@@ -51,10 +40,50 @@ public class Main {
         }
 
         int trueResult = 0;
-        for (int i = 1; i <= arr.length + 1; i++) {
+
+        int cnt = 0;
+        int i = 1;
+        if (arr.length > 0) {
+            i = arr[0];
+        }
+        while (cnt < arr.length + 1) {
             trueResult += i;
+            i++;
+            cnt++;
+        }
+        return trueResult - result;
+    }
+
+    public static void test(Map<Integer[], Integer> map) {
+        System.out.println("Тест2");
+        for (Map.Entry<Integer[], Integer> o : map.entrySet()) {
+            Integer[] arr = o.getKey();
+            Integer expected = o.getValue();
+
+            arr = smartSearch(arr);
+            System.out.println("Ожидаемый результат: " + expected + ", Полученный результат: " + arr[0]);
+        }
+    }
+
+    public static Integer[] smartSearch(Integer[] arr) {
+        if (arr.length < 3) {
+            int temp = actualSum(arr);
+            Integer[] result = new Integer[1];
+            result[0] = temp;
+            return result;
         }
 
-        return trueResult - result;
+        int idx = arr[0] - 1;
+        int head = 0;
+        int base = 1;
+        int j = 0;
+
+        while (base + idx == arr[base - 1]) {
+            head = base - 1;
+            base *= 2;
+            if (base > arr.length) break;
+        }
+
+        return smartSearch(Arrays.copyOfRange(arr, head, base));
     }
 }
